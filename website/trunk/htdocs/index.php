d46cd71c39b07cf05731e941ee167e0ec383b678
David Schmidt <david-schmidt@users.noreply.github.com>
2017-08-18 18:56:49 -0400

    Initial reorganization in git (#1)
    * Initial reorganization in git
    * Interim updates for Windows build
    * Successful build on Windows

--- website/trunk/htdocs/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('setup.php');
-
-$smarty = createSmarty();
-$smarty->assign('title', "Apple Disk Transfer");
-$smarty->layout('index.markdown');
-
-?>
--- website/trunk/htdocs/sst_dissection/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('setup.php');
-
-$smarty = createSmarty();
-$smarty->assign('title', "SST Dissection");
-$smarty->layout('sst_dissection.markdown');
-
-?>
